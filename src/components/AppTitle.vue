020db6602e0e6b86b7dffcc3724bd690ab9d0586
Lapo0 <lapo.gini.01@gmail.com>
2023-02-28 16:46:47 +0100

    FEAT AppTitle && AppHero

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>
+            {{ title }}
+        </h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: 'La mia prima App con Vite!'
+            }
+        },
+    }
+</script>
